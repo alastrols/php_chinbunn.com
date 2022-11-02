85b9331e5289c39fb7e58a96ce7abd9321699e18
Suphavit Bunnag <alastrol@MacBook-Air-khxng-Suphavit.local>
2022-11-02 22:00:14 +0700

    ..

--- src/Main/Controller/IndexController.php
+++ src/Main/Controller/IndexController.php
@@ -12,23 +12,24 @@ class IndexController extends BaseController
     public function getIndex(Request $req, Response $res)
     {
         $container = $this->ci;
-        $session = $container->session;
-        $segment = $session->getSegment("views");
-        $ip = $_SERVER['REMOTE_ADDR'];
-        $db = $this->ci->medoo;
-        $checkexist = $db->get('views',['ipaddress'],['ipaddress'=>$ip]);
-        if($checkexist == false){
-        $getIpaddress = $db->insert('views',['ipaddress'=>$ip]);
-        }
-        $views = $db->select('views',['ipaddress']);
-        $viewcount = count($views);
-        $segment->set('counter', $viewcount);
+        // $session = $container->session;
+        // $segment = $session->getSegment("views");
+        // $ip = $_SERVER['REMOTE_ADDR'];
+        // $db = $this->ci->medoo;
+        // $checkexist = $db->get('views',['ipaddress'],['ipaddress'=>$ip]);
+        // if($checkexist == false){
+        // $getIpaddress = $db->insert('views',['ipaddress'=>$ip]);
+        // }
+        // $views = $db->select('views',['ipaddress']);
+        // $viewcount = count($views);
+        // $segment->set('counter', $viewcount);
 
         // if ($req->getUri()->getScheme() !== 'https') {
         // $uri = $req->getUri()->withScheme("https")->withPort(null);
         // return $res->withRedirect( (string)$uri );
         // } else {
-        return $this->ci->view->render($res, 'index.twig',['viewcount'=>$viewcount]);
+        // return $this->ci->view->render($res, 'index.twig',['viewcount'=>$viewcount]);
+        return $this->ci->view->render($res, 'index.twig');
         // }
         
     }
